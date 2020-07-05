e54b8809442c3702c1e9e619b824c95dac4802d1
Tonyzczc <718558450@qq.com>
2020-07-05 17:40:38 +0800

    Ribbon \ Feign 服务调用

---
+++ client_customer/src/main/java/com/cloud/client/client_customer/ClientCustomerApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.client.client_customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientCustomerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientCustomerApplication.class, args);
+	}
+
+}
---
+++ client_customer/src/test/java/com/cloud/client/client_customer/ClientCustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.client.client_customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientCustomerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
